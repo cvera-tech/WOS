aed08a5d49b0f60a90664c3513627c84c741aa6d
jbvera <johnbenedictv@gmail.com>
2019-08-15 16:06:35 -0400

    Implement removedir

---
+++ Utilities/removedir/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("USAGE: removedir <directory name>");
+            }
+            else
+            {
+                string directoryName = args[0];
+                if (!Directory.Exists(directoryName))
+                {
+                    if (File.Exists(directoryName))
+                    {
+                        Console.Error.WriteLine($"removedir: {directoryName}: not a directory");
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"removedir: {directoryName}: directory does not exist");
+                    }
+                }
+                else
+                {
+                    var directoryFiles = Directory.EnumerateFileSystemEntries(directoryName);
+                    if (directoryFiles.Count() != 0)
+                    {
+                        Console.Error.WriteLine($"removedir: {directoryName}: directory not empty");
+                    }
+                    else
+                    {
+                        Directory.Delete(directoryName);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
