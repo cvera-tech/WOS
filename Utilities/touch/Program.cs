ab627f554081f37dae74b1a0140bcffea12409ab
jbvera <johnbenedictv@gmail.com>
2019-08-15 12:24:11 -0400

    Implement touch

---
+++ Utilities/touch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace touch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO:
+            // If file does not exist, create file
+            // If file exists, set LastWrittenTo to current time
+
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("USAGE: touch <file name>");
+            }
+            else
+            {
+                string fileName = args[0];
+                if (File.Exists(fileName))
+                {
+                    var fileInfo = new FileInfo(fileName);
+                    fileInfo.LastWriteTime = DateTime.Now;
+                }
+                else
+                {
+                    using (File.Create(fileName)) {}
+                }
+            }
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
