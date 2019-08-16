6b2982abe51695a49bb43e7c7d7deff413d2a127
jbvera <johnbenedictv@gmail.com>
2019-08-16 09:04:55 -0400

    Start grep

---
+++ Utilities/grep/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("USAGE: grep <pattern> <path>");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
