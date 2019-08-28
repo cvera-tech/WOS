7cebee973f2d18b0e888b3b9d8705551131799ca
jbvera <johnbenedictv@gmail.com>
2019-08-28 17:05:50 -0400

    Add EditAuthor

---
+++ LibraryApplication/LibraryApplication/EditAuthor.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EditAuthor.aspx.cs" Inherits="LibraryApplication.EditAuthor" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
+    <div>
+        <asp:Label ID="OldFirstName" runat="server" />
+    </div>
+    <div>
+        <asp:Label ID="OldLastName" runat="server" />
+    </div>
+    <fieldset>
+        <asp:ValidationSummary runat="server"
+            EnableClientScript="true" />
+
+        <div>
+            <asp:Label runat="server"
+                AssociatedControlID="NewFirstName"
+                Text="New First Name: " />
+            <asp:TextBox ID="NewFirstName" runat="server" />
+            <asp:RequiredFieldValidator runat="server"
+                ControlToValidate="NewFirstName"
+                EnableClientScript="true"
+                Display="Dynamic"
+                ErrorMessage="First name required."
+                Text="*" />
+        </div>
+        <div>
+            <asp:Label runat="server"
+                AssociatedControlID="NewLastName"
+                Text="New Last Name: " />
+            <asp:TextBox ID="NewLastName" runat="server" />
+            <asp:RequiredFieldValidator runat="server"
+                ControlToValidate="NewLastName"
+                EnableClientScript="true"
+                Display="Dynamic"
+                ErrorMessage="Last name required."
+                Text="*" />
+        </div>
+        <div>
+            <asp:Button ID="SaveButton" runat="server" OnCommand="SaveButton_Command" Text="Save" />
+            <asp:Button ID="CancelButton" runat="server" OnCommand="CancelButton_Command" Text="Cancel" />
+        </div>
+    </fieldset>
+</asp:Content>
---
+++ LibraryApplication/LibraryApplication/EditAuthor.aspx.cs
@@ -0,0 +1,81 @@
+﻿using Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibraryApplication
+{
+    public partial class EditAuthor : System.Web.UI.Page
+    {
+        private const string AuthorsUrl = "~/Authors.aspx";
+        private const string SqlSelectQuery = @"
+            SELECT FirstName, LastName
+            FROM Author
+            WHERE Id = @AuthorId
+        ";
+        private const string SqlUpdateQuery = @"
+            UPDATE Author
+            SET
+                FirstName = @FirstName,
+                LastName = @LastName
+            WHERE Id = @AuthorId
+        ";
+
+        private int authorId;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!int.TryParse(Request.QueryString["ID"], out authorId))
+            {
+                Response.Redirect(AuthorsUrl);
+            }
+
+            if (!IsPostBack)
+            {
+                DataTable dt = DatabaseHelper.Retrieve(
+                    SqlSelectQuery,
+                    new SqlParameter("@AuthorId", authorId));
+
+                if (dt.Rows.Count == 1)
+                {
+                    string oldFirstName = dt.Rows[0].Field<string>("FirstName");
+                    string oldLastName = dt.Rows[0].Field<string>("LastName");
+                    OldFirstName.Text = $"Current First Name: {oldFirstName}";
+                    OldLastName.Text = $"Current Last Name: {oldLastName}";
+                    NewFirstName.Text = oldFirstName;
+                    NewLastName.Text = oldLastName;
+                }
+                else
+                {
+                    Response.Redirect(AuthorsUrl);
+                }
+            }
+        }
+
+        protected void SaveButton_Command(object sender, CommandEventArgs e)
+        {
+            string newFirstName = NewFirstName.Text;
+            string newLastName = NewLastName.Text;
+
+            DatabaseHelper.Update(
+                SqlUpdateQuery,
+                new SqlParameter("@FirstName", newFirstName),
+                new SqlParameter("@LastName", newLastName),
+                new SqlParameter("@AuthorId", authorId)
+            );
+
+            // TODO: Update query
+            Response.Redirect(AuthorsUrl);
+        }
+
+        protected void CancelButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect(AuthorsUrl);
+        }
+    }
+}
