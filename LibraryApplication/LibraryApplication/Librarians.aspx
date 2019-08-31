d96e3fefa6098a4123a55ac2f8d314cb20379b19
jbvera <johnbenedictv@gmail.com>
2019-08-30 20:10:43 -0400

    Add Librarians page

---
+++ LibraryApplication/LibraryApplication/Librarians.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Librarians.aspx.cs" Inherits="LibraryApplication.Librarians" %>
+
+<%@ Import Namespace="System.Data" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
+    <asp:Repeater ID="LibrariansRepeater" runat="server"
+        ItemType="DataRow">
+        <HeaderTemplate>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Library</th>
+                        <th>Address</th>
+                        <th>Email Address</th>
+                    </tr>
+                </thead>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <tr>
+                <td><%# Item.Field<string>("FirstName") %></td>
+                <td><%# Item.Field<string>("LastName") %></td>
+                <td><%# Item.Field<string>("LibraryName") %></td>
+                <td><%# Item.Field<string>("Address") %></td>
+                <td><%# Item.Field<string>("EmailAddress") %></td>
+            </tr>
+        </ItemTemplate>
+        <FooterTemplate>
+            </table>
+        </FooterTemplate>
+    </asp:Repeater>
+    <div>
+        <asp:Button ID="AddButton" runat="server" 
+            OnCommand="AddButton_Command"
+            Text="Add Librarian" />
+    </div>
+</asp:Content>
---
+++ LibraryApplication/LibraryApplication/Librarians.aspx.cs
@@ -0,0 +1,50 @@
+﻿using Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibraryApplication
+{
+    public partial class Librarians : System.Web.UI.Page
+    {
+        private const string AddLibrarianUrl = "~/AddLibrarian.aspx";
+        private const string GetLibrariansQuery = @"
+            SELECT 
+	            E.FirstName,
+	            E.LastName,
+	            L2.Name AS LibraryName,
+	            E.AddressLine1 + 
+		            ISNULL(' ' + E.AddressLine2, '') + 
+		            ', ' + E.City +
+		            ', ' + S.Name +
+		            ' ' + E.PostalCode
+		            AS Address,
+	            E.EmailAddress
+            FROM Librarian L1
+	            JOIN Employee E
+		            ON L1.EmployeeId = E.Id
+	            JOIN Library L2
+		            ON E.LibraryId = L2.Id
+	            JOIN State s
+		            ON E.StateId = S.Id
+        ";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataTable librariansTable = DatabaseHelper.Retrieve(GetLibrariansQuery);
+                LibrariansRepeater.DataSource = librariansTable.Rows;
+                LibrariansRepeater.DataBind();
+            }
+        }
+
+        protected void AddButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect(AddLibrarianUrl);
+        }
+    }
+}
