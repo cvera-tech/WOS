c41bf996da7a2491850d4e30d52eb42e08bb9a98
jbvera <johnbenedictv@gmail.com>
2019-09-09 15:49:53 -0400

    Add Create page and Twits display in Index page

--- Twits/Twits/Controllers/DatabaseHelper.cs
+++ Twits/Twits/Controllers/DatabaseHelper.cs
@@ -7,7 +7,7 @@ namespace Twits.Controllers
 {
     public static class DatabaseHelper
     {
-        private const string DatabaseName = "TwitApp";
+        private const string DatabaseName = "TwitDatabase";
         public static DataTable Retrieve(string sql, params SqlParameter[] parameters)
         {
             string ConnectionString = ConfigurationManager.ConnectionStrings[DatabaseName].ConnectionString;
@@ -100,6 +100,30 @@ namespace Twits.Controllers
         public static SqlParameter GetNullableStringSqlParameter(string parameterName, string value)
         {
             return new SqlParameter(parameterName, string.IsNullOrWhiteSpace(value) ? (object)DBNull.Value : value);
+        }
+
+        /// <summary>
+        /// This doesn't work because the connection is closed upon return
+        /// </summary>
+        public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] parameters)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[DatabaseName].ConnectionString;
+            SqlDataReader dataReader;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(sql, connection);
+
+                foreach (var parameter in parameters)
+                {
+                    command.Parameters.Add(parameter);
+                }
+
+                // This is where the work is happening.
+                command.Connection.Open();
+                dataReader = command.ExecuteReader();
+            }
+            return dataReader;
         }
     }
 }
--- Twits/Twits/Controllers/TwitController.cs
+++ Twits/Twits/Controllers/TwitController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 using Twits.Models;
 
@@ -9,7 +12,27 @@ namespace Twits.Controllers
         // GET: Twit
         public ActionResult Index()
         {
+            string sql = @"
+                SELECT Id, Text, CreatedOn
+                FROM Twits
+                ORDER BY CreatedOn DESC
+                OFFSET 0 ROWS
+                FETCH NEXT 100 ROWS ONLY
+            ";
+
             List<Twit> twits = new List<Twit>();
+            DataTable twitsTable = DatabaseHelper.Retrieve(sql);
+
+            foreach (DataRow row in twitsTable.Rows)
+            {
+                Twit twit = new Twit
+                {
+                    Id = row.Field<int>("Id"),
+                    Text = row.Field<string>("Text"),
+                    CreatedOn = row.Field<DateTimeOffset>("CreatedOn")
+                };
+                twits.Add(twit);
+            }
             return View(twits);
         }
 
@@ -19,27 +42,38 @@ namespace Twits.Controllers
         //    return View();
         //}
 
-        //// GET: Twit/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+        // GET: Twit/Create
+        public ActionResult Create()
+        {
+            Twit twit = new Twit();
+            return View(twit);
+        }
 
-        //// POST: Twit/Create
-        //[HttpPost]
-        //public ActionResult Create(FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add insert logic here
+        // POST: Twit/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            Twit twit = new Twit();
+            twit.Text = collection.Get("Text");
+            twit.CreatedOn = DateTimeOffset.Now;
+            try
+            {
+                string sql = @"
+                    INSERT INTO Twits (Text, CreatedOn)
+                    VALUES (@Text, @CreatedOn)
+                ";
 
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+                DatabaseHelper.Insert(sql,
+                    new SqlParameter("@Text", twit.Text),
+                    new SqlParameter("@CreatedOn", twit.CreatedOn));
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(twit);
+            }
+        }
 
         //// GET: Twit/Edit/5
         //public ActionResult Edit(int id)
