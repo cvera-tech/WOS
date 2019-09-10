1a150d9f811624ae68fe64742ba1c025bf1a65f6
jbvera <johnbenedictv@gmail.com>
2019-09-10 16:28:40 -0400

    Add models and DatabaseHelper class

---
+++ Blahgger/Blahgger/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blahgger.Models
+{
+    public class Post
+    {
+        public string Text { get; set; }
+
+        [Display(Name = "Created On")]
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public int UsersId { get; set; }
+    }
+}
