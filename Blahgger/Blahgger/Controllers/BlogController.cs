618f0f23c915d2230e756d4fade18554b635ff8d
jbvera <johnbenedictv@gmail.com>
2019-09-10 16:05:03 -0400

    Blahgger initial commit

---
+++ Blahgger/Blahgger/Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blahgger.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: 
+        public ActionResult CreatePost()
+        {
+            return View();
+        }
+
+        public ActionResult EditPost()
+        {
+            return View();
+        }
+
+        public ActionResult AddComment()
+        {
+            return View();
+        }
+
+        public ActionResult EditComment()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteComment()
+        {
+            return View();
+        }
+    }
+}
