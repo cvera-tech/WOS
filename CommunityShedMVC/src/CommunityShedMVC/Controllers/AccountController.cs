6c1418e649a076c94991931cb95c9e34399905be
jbvera <johnbenedictv@gmail.com>
2019-09-11 12:21:26 -0400

    Add AccountsController and Login page

---
+++ CommunityShedMVC/src/CommunityShedMVC/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using CommunityShedMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityShedMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            var viewModel = new LoginViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
