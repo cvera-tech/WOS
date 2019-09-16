5c56c7c5f2c0fda2f4a9e2e87b865368410235b9
jbvera <johnbenedictv@gmail.com>
2019-09-16 10:21:06 -0400

    InvoiceMaker initial commit

---
+++ InvoiceMaker/InvoiceMaker/MD5HashHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+namespace InvoiceMaker
+{
+    public class Md5HashHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("Hey!");
+        }
+    }
+}
