484da2122975d0b594421ca7499426b740cbf7ca
jbvera <johnbenedictv@gmail.com>
2019-09-16 11:14:14 -0400

    Add MD5 hashing module

---
+++ InvoiceMaker/InvoiceMaker/Md5HashModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+
+namespace InvoiceMaker
+{
+    public class Md5HashModule : IHttpModule
+    {
+        public void Dispose() { }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += HandleBeginRequest;
+        }
+
+        private void HandleBeginRequest(object sender, EventArgs e)
+        {
+            InvoiceMakerApplication application = sender as InvoiceMakerApplication;
+            HttpContext context = application.Context;
+            HttpRequest request = context.Request;
+            Uri uri = request.Url;
+            string absolutePath = uri.AbsolutePath;
+            string newPath = "/";
+            if (absolutePath.StartsWith("/api/hash/"))
+            {
+                newPath += $"{absolutePath.Substring(10)}.hash";
+                context.RewritePath(newPath);
+            }
+            else if (absolutePath.StartsWith("/api/binhash/"))
+            {
+                newPath += $"{absolutePath.Substring(13)}.binhash";
+                context.RewritePath(newPath);
+            }
+        }
+    }
+}
