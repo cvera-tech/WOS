ef6de7460d5ba36a0a4d42aa7c5dcd05ed0fb1d5
jbvera <johnbenedictv@gmail.com>
2019-09-18 14:07:13 -0400

    Add extension methods

--- EntityFrameworkDemo/EntityFrameworkDemo/Models/Person.cs
+++ EntityFrameworkDemo/EntityFrameworkDemo/Models/Person.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkDemo.Models
 {
-    class Person
+    internal class Person
     {
         // Id, ID, PersonId, PersonID
         // Automatically set as primary key and 
         // identity column if `int` or `guid`
         public int Id { get; set; }
-        [Required]
+        [Required, MaxLength(100)]
         public string Name { get; set; }
-        public DateTime? BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public decimal Weight { get; set; }
         public bool IsStarWarsFan { get; set; }
 
@@ -27,5 +27,9 @@ namespace EntityFrameworkDemo.Models
             }
         }
 
+        public override string ToString()
+        {
+            return $"Name: {Name}\nBirthDate: {BirthDate.ToShortDateString()}\nWeight: {Weight}\nIsStarWarsFan: {IsStarWarsFan}";
+        }
     }
 }
--- EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
+++ EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -3,7 +3,9 @@ using EntityFrameworkDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,22 +19,44 @@ namespace EntityFrameworkDemo
             Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
             //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
 
-            using (var context = new Context())
-            {
-                //AddPeople(context);
-                context.Database.Log = (message) => Console.WriteLine(message);
+            var context = new Context();
+            context.Database.Log = (message) => Console.WriteLine(message);
 
-                // SELECT * FROM People WHERE IsStarWarsFan = 1
-                //var people = context.People.ToList();
-                //people.ForEach(p => Console.WriteLine(p.Name));
-                var filteredPeople = context.People.Where(IsStarWarsFan).ToList();
-                filteredPeople.ForEach(p => Console.WriteLine(p.Name));
+
+            using (context)
+            {
+
+                if (context.People.Count() == 0)
+                {
+                    AddPeople(context);
+                }
+
+                //var filteredPeople = context.People.Where(IsStarWarsFan).ToList();
+                //filteredPeople.ForEach(p => Console.WriteLine(p));
+
+                //var john = context.People.Where(p => p.Name == "John").First();
+                //var entry = context.Entry(john);
+
+                //john.Weight = 190;
+                //var entry2 = context.Entry(john);
+                SelectPeople(context);
+                context.DetachAllEntities();
+                context.UpdateById<Person>(1, "Name", "John", "Weight", (decimal)2000);
+                context.DetachAllEntities();
+                SelectPeople(context);
             }
             Console.ReadKey();
         }
 
-        static void AddPeople(Context context)
+        private static void AddPeople(Context context)
         {
+            context.People.Add(new Person()
+            {
+                Name = "John",
+                BirthDate = new DateTime(1995, 4, 24),
+                Weight = 160,
+                IsStarWarsFan = true
+            });
             context.People.Add(new Person()
             {
                 Name = "John2",
@@ -40,13 +64,80 @@ namespace EntityFrameworkDemo
                 Weight = 160,
                 IsStarWarsFan = false
             });
+            context.SaveChanges();
 
+        }
+
+        private static bool IsStarWarsFan(Person person)
+        {
+            return person.IsStarWarsFan;
+        }
+
+        private static void DeleteById(Context context, int id)
+        {
+            var person = new Person() { Id = id };
+            context.Entry(person).State = EntityState.Deleted;
             context.SaveChanges();
         }
 
-        static bool IsStarWarsFan(Person person)
+        private static void SelectPeople(Context context)
         {
-            return person.IsStarWarsFan;
+            //SELECT * FROM People
+            var people = context.People.ToList();
+            people.ForEach(p => Console.WriteLine(p));
+        }
+
+        private static void SelectPeople(Context context, Func<Person, bool> predicate)
+        {
+            var people = context.People.Where(predicate).ToList();
+            people.ForEach(p => Console.WriteLine(p));
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        internal static void DetachAllEntities(this Context context)
+        {
+            var changedEntries = context.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added ||
+                            e.State == EntityState.Modified ||
+                            e.State == EntityState.Deleted)
+                .ToList();
+
+            foreach (var entry in changedEntries)
+            {
+                entry.State = EntityState.Detached;
+            }
+        }
+        internal static void UpdateById<EntityType>(this Context context, int id, params object[] parameters)
+        {
+            if (parameters.Count() % 2 != 0)
+            {
+                throw new ArgumentException("Invalid number of parameters");
+            }
+
+            Type entityType = typeof(EntityType);
+            EntityType entity = Activator.CreateInstance<EntityType>();
+            entityType.GetProperty("Id").SetValue(entity, id);
+            var set = context.Set(entityType);
+            set.Attach(entity);
+            for (int index = 0; index < parameters.Length; index += 2)
+            {
+                var parameterName = (string)parameters[index];
+                var parameterValue = parameters[index + 1];
+
+                entityType.GetProperty(parameterName).SetValue(entity, parameterValue);
+                context.Entry((object)entity).Property(parameterName).IsModified = true;
+            }
+            try
+            {
+                context.SaveChanges();
+
+            }
+            catch (DbEntityValidationException e)
+            {
+                Console.WriteLine("What the heck");
+            }
         }
     }
 }
