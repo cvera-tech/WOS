8b20d2bf0588e9c4a5a531072232173f5949bd4c
jbvera <johnbenedictv@gmail.com>
2019-09-18 11:04:18 -0400

    EntityFrameworkDemo initial commit

---
+++ EntityFrameworkDemo/EntityFrameworkDemo/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo.Models
+{
+    class Person
+    {
+        // Id, ID, PersonId, PersonID
+        // Automatically set as primary key and 
+        // identity column if `int` or `guid`
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public decimal Weight { get; set; }
+        public bool IsStarWarsFan { get; set; }
+
+        public string Info
+        {
+            get
+            {
+                return $"{Name} ({BirthDate}";
+            }
+        }
+
+    }
+}
---
+++ EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using EntityFrameworkDemo.Data;
+using EntityFrameworkDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+
+            using (var context = new Context())
+            {
+                //AddPeople(context);
+                context.Database.Log = (message) => Console.WriteLine(message);
+
+                // SELECT * FROM People WHERE IsStarWarsFan = 1
+                //var people = context.People.ToList();
+                //people.ForEach(p => Console.WriteLine(p.Name));
+                var filteredPeople = context.People.Where(IsStarWarsFan).ToList();
+                filteredPeople.ForEach(p => Console.WriteLine(p.Name));
+            }
+            Console.ReadKey();
+        }
+
+        static void AddPeople(Context context)
+        {
+            context.People.Add(new Person()
+            {
+                Name = "John2",
+                BirthDate = new DateTime(1995, 4, 24),
+                Weight = 160,
+                IsStarWarsFan = false
+            });
+
+            context.SaveChanges();
+        }
+
+        static bool IsStarWarsFan(Person person)
+        {
+            return person.IsStarWarsFan;
+        }
+    }
+}
