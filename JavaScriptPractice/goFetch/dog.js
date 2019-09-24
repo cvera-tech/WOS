5115c152a9da489d6a6b80185d41eb37f5415dcb
jbvera <johnbenedictv@gmail.com>
2019-09-24 17:40:47 -0400

    Go Fetch phase 2

--- JavaScriptPractice/goFetch/dog.js
+++ JavaScriptPractice/goFetch/dog.js
@@ -1,3 +1,11 @@
+function setDogImage(url) {
+    document.getElementById("dog-image").setAttribute("src", url);
+}
+
+function setErrorMessage(message) {
+    document.getElementById("error-message").textContent = message;
+}
+
 function makeDogRequest() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
@@ -9,8 +17,33 @@ function makeDogRequest() {
     xhr.send();
 }
 
-function setDogImage(url) {
-    document.getElementById("dog-image").setAttribute("src", url);
+function makeRequest(HTTPVerb, url) {
+    const promise = new Promise(
+        function (resolve, reject) {
+            const xhr = new XMLHttpRequest();
+            xhr.open(HTTPVerb, url);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    let response = JSON.parse(this.responseText);
+                    resolve(response);
+                }
+                else {
+                    let error = JSON.parse(this.responseText);
+                    reject(error.message);
+                }
+            };
+            xhr.onerror = function() {
+                reject("TRANSACTION ERROR");
+            };
+            xhr.send();
+        }
+    );
+    return promise;
 }
 
-makeDogRequest();
+makeRequest("POST", "og.ceo/api/breeds/image/xyzzy")
+    .then(response => {
+        setDogImage(response.message);
+        return response;
+    })
+    .catch(message => setErrorMessage(message));//);
